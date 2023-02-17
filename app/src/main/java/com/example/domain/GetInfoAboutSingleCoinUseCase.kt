e21441553e5badf629ee3a7a3089af4403404598
asenanov7 <asenanov7@gmail.com>
2023-02-17 22:00:30 +0300

    7.1 Small Review FINAL

--- app/src/main/java/com/example/domain/GetInfoAboutSingleCoinUseCase.kt
+++ app/src/main/java/com/example/domain/GetInfoAboutSingleCoinUseCase.kt
@@ -2,7 +2,7 @@ package com.example.domain
 
 import androidx.lifecycle.LiveData
 
-class GetInfoAboutSingleCoin(private val repository: Repository) {
+class GetInfoAboutSingleCoinUseCase(private val repository: Repository) {
     suspend operator fun invoke(coinSym:String): LiveData<CoinPriceInfo> {
         return repository.getDetailInfoAboutSingleCoin(coinSym)
     }
--- app/src/main/java/com/example/presentation/viewmodels/DetailViewModel.kt
+++ app/src/main/java/com/example/presentation/viewmodels/DetailViewModel.kt
@@ -5,15 +5,15 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import com.example.data.RepositoryImpl
 import com.example.domain.CoinPriceInfo
-import com.example.domain.GetInfoAboutSingleCoin
+import com.example.domain.GetInfoAboutSingleCoinUseCase
 
 class DetailViewModel(application: Application):AndroidViewModel(application) {
 
     private val repository = RepositoryImpl(application)
-    private val getInfoAboutSingleCoin = GetInfoAboutSingleCoin(repository)
+    private val getInfoAboutSingleCoinUseCase = GetInfoAboutSingleCoinUseCase(repository)
 
     suspend fun getInfoAboutSingleCoinLD(coinSym:String): LiveData<CoinPriceInfo> {
-       return getInfoAboutSingleCoin(coinSym)
+       return getInfoAboutSingleCoinUseCase(coinSym)
     }
 
 
