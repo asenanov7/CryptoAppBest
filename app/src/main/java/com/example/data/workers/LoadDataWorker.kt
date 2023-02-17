f12f0e27ffc4af16e8022e58aaf05bad28fde547
asenanov7 <asenanov7@gmail.com>
2023-02-18 01:11:48 +0300

    7.2 Small Review

--- app/src/main/java/com/example/data/workers/LoadDataWorker.kt
+++ app/src/main/java/com/example/data/workers/LoadDataWorker.kt
@@ -4,17 +4,21 @@ import android.content.Context
 import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.OneTimeWorkRequest
+import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.example.data.database.MapperDB
 import com.example.data.database.room.DatabaseCoins
 import com.example.data.network.MapperDTO
 import com.example.data.network.retrofit.ApiFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class LoadDataWorker(
     context: Context,
     workerParameters: WorkerParameters,
-) : CoroutineWorker(context, workerParameters) {
+) : Worker(context, workerParameters) {
 
     private val api = ApiFactory.apiService
     private val dao = DatabaseCoins.getInstance(context).getDao()
@@ -22,32 +26,36 @@ class LoadDataWorker(
     private val mapperDB = MapperDB()
     private val mapperDTO = MapperDTO()
 
-    override suspend fun doWork(): Result {
-        while (true) {
-            try {
-                Log.d("ARSEN", "do work: loadData: ")
-                val dtoListCoins = api.getTopCoins().listOfCoins
-                val namesOfCoins = mapperDTO.mapDtoCoinNameListToString(dtoListCoins)
+    override fun doWork(): Result {
+        val scope = CoroutineScope(Dispatchers.IO)
+        scope.launch {
+            while (true) {
+                try {
+                    Log.d("ARSEN", "do work: loadData: ")
+                    val dtoListCoins = api.getTopCoins().listOfCoins
+                    val namesOfCoins = mapperDTO.mapDtoCoinNameListToString(dtoListCoins)
 
-                val dtoJsonContainer =
-                    api.getFullPriceList(fromSym = namesOfCoins)
+                    val dtoJsonContainer =
+                        api.getFullPriceList(fromSym = namesOfCoins)
 
-                val listDtoCoinPriceInfo =
-                    mapperDTO.mapJsonContainerToListCoinInfo(dtoJsonContainer)
+                    val listDtoCoinPriceInfo =
+                        mapperDTO.mapJsonContainerToListCoinInfo(dtoJsonContainer)
 
-                val listCoinPriceInfo =
-                    listDtoCoinPriceInfo.map { mapperDTO.mapDtoToEntity(it) }
+                    val listCoinPriceInfo =
+                        listDtoCoinPriceInfo.map { mapperDTO.mapDtoToEntity(it) }
 
-                val listModelCoinPriceInfo =
-                    mapperDB.mapListEntityToListDBModelCoinPriceInfo(listCoinPriceInfo)
+                    val listModelCoinPriceInfo =
+                        mapperDB.mapListEntityToListDBModelCoinPriceInfo(listCoinPriceInfo)
 
-                dao.insertDataOnDatabase(listModelCoinPriceInfo)
-            } catch (_: Exception) {
+                    dao.insertDataOnDatabase(listModelCoinPriceInfo)
+                } catch (_: Exception) {
+                }
+
+                delay(10000)
             }
-
-            delay(10000)
         }
-
+        Log.d("ARSEN", "мы сюда дошли")
+        return Result.retry()
     }
 
     companion object {
